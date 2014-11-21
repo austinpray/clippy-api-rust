ee160d24db595ccf43f65419006e667c767ef0ed
Ridwan Hoq <ridwanhoq@gmail.com>
2014-11-20 22:17:47 -0600

    add /hello/:thing route

---
+++ README.md
@@ -0,0 +1,11 @@
+#clippy-api-rust
+
+##Installation
+You’ll need the nightly version of [Rust](http://www.rust-lang.org/install.html) which will also install Cargo (Rust’s package manager)
+
+1. Clone the repo and cd into it
+2. Run ``` $ cargo run ``` and the server should be running on http://127.0.0.1:6767/
+
+##Available routes
+###/hello/:thing
+Returns “Hello, :thing!”
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate serialize;
+extern crate nickel;
+extern crate http;
+
+use nickel::{Nickel, Request, Response, HttpRouter};
+use std::io::net::ip::Ipv4Addr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+    let mut router = Nickel::router();
+
+    fn hello_handler(request: &Request, response: &mut Response) {
+        let hello_thing = format!("Hello {}!", request.param("thing"));
+        response.send(hello_thing.as_slice());
+    }
+
+    router.get("/hello/:thing", hello_handler);
+
+    server.utilize(router);
+
+    server.listen(Ipv4Addr(127, 0, 0, 1), 6767);
 }
